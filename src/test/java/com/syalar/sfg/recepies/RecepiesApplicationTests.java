f25e5ec42ad414845e772742b5e45d676a4e1743
Juan David Rodriguez Romero <jd.rodriguez@globant.com>
2019-10-18 10:32:10 -0500

    Initial commit

---
+++ src/test/java/com/syalar/sfg/recepies/RecepiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syalar.sfg.recepies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecepiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
