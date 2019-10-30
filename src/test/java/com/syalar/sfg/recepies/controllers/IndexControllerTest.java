f5127303f5ad0b880e00144369d56b99b0272fb0
Juan David Rodriguez Romero <jd.rodriguez@globant.com>
2019-10-30 12:07:14 -0500

    Mockito Argument Capture

--- src/test/java/com/syalar/sfg/recepies/controllers/IndexControllerTest.java
+++ src/test/java/com/syalar/sfg/recepies/controllers/IndexControllerTest.java
@@ -1,11 +1,16 @@
 package com.syalar.sfg.recepies.controllers;
 
+import com.syalar.sfg.recepies.domain.Recipe;
 import com.syalar.sfg.recepies.services.RecipeService;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
@@ -29,10 +34,26 @@ public class IndexControllerTest {
 
     @Test
     public void getIndexPage() {
+
+        //given
+        Set<Recipe> recipes = new HashSet<>();
+        recipes.add(new Recipe());
+        Recipe recipe = new Recipe();
+        recipe.setId(2L);
+        recipes.add(recipe);
+
+        when(recipeService.findAll()).thenReturn(recipes);
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+        //when
         String viewName = indexController.getIndexPage(model);
+
+        //then
         assertEquals(viewName, "index");
         verify(recipeService, times(1)).findAll();
-        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertEquals(2, setInController.size());
 
     }
 }
